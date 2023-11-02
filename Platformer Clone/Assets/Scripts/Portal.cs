f040424aed5196cb79635d4872140f42229f5ff2
crocodisle <crocodisle@gmail.com>
2023-11-02 08:40:13 -0700

    Added a portal prefab.

--- Platformer Clone/Assets/Scripts/Portal.cs
+++ Platformer Clone/Assets/Scripts/Portal.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: [McGee, Patrick]
+ * Last Updated: [11/02/23]
+ * [Contains a reference to the public spawnPoint GameObject for determining where players will spawn when they enter a portal. Script should be attached to a Portal object.]
+ */
+
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject spawnPoint;
 }
