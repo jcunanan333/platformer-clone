1d45812e8a074645c4f4b8de7a2466ca87b3c699
crocodisle <crocodisle@gmail.com>
2023-11-02 08:59:19 -0700

    Added prefabs for RegularEnemy and HardEnemy.

--- Platformer Clone/Assets/Scripts/HardEnemy.cs
+++ Platformer Clone/Assets/Scripts/HardEnemy.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: [McGee, Patrick]
+ * Last Updated: [11/02/2023]
+ * [This file contains the code that determines the behavior for HardEnemy objects.]
+ */
+
 public class HardEnemy : MonoBehaviour
 {
+    public int damageDealt = 7;
+
     // Start is called before the first frame update
     void Start()
     {
--- Platformer Clone/Assets/Scripts/RegularEnemy.cs
+++ Platformer Clone/Assets/Scripts/RegularEnemy.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * Author: [McGee, Patrick]
+ * Last Updated: [11/02/2023]
+ * [This file conatins the code that determines the behavior for RegularEnemy objects.]
+ */
+
+
 public class RegularEnemy : MonoBehaviour
 {
+    public int damageDealt = 5;
+
+
     // Start is called before the first frame update
     void Start()
     {
