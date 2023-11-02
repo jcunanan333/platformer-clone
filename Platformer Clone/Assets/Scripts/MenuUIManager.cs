be10fe6731b91a4ab4ec76ccf8b2c8d5fcac449c
crocodisle <crocodisle@gmail.com>
2023-11-02 09:22:01 -0700

    Added Menu UI and edited script to handle UI buttons in GameOver and MainMenu scenes. Also added scenes to build list for SceneManager.

--- Platformer Clone/Assets/Scripts/MenuUIManager.cs
+++ Platformer Clone/Assets/Scripts/MenuUIManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScene(int sceneIndex)
     {
-        
+        SceneManager.LoadScene(sceneIndex);
     }
 }
